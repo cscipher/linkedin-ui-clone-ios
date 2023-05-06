8cc791a2d0b0c053d7e688f04b2162f3aa6ba56c
Harsh Soni <harsh.here17@gmail.com>
2023-05-06 14:16:36 +0530

    Initial Commit

---
+++ LinkedInClone/LinkedInCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedInCloneApp.swift
+//  LinkedInClone
+//
+//  Created by Harsh Soni on 06/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LinkedInCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
